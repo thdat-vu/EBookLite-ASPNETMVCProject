97488531db988dbf8d263467a221d164cab68f47
Thanh Dat Vu <vuthanhdat181@gmail.com>
2024-03-03 16:52:02 +0700

    Create Roles in Database + Create fake EmailSender Class

--- Bulky/Program.cs
+++ Bulky/Program.cs
@@ -4,6 +4,8 @@ using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Bulky.Ultility;
 
 namespace Bulky
 {
@@ -19,11 +21,12 @@ namespace Bulky
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
             //add pipeline for Identity bcause it is written in RazorPage
             builder.Services.AddRazorPages();
             //register dependency injection into container
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
             //build app
             var app = builder.Build();
 
