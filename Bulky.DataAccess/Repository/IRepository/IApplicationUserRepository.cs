65405751353eb4a6636d0ef32f4f4866d90dfb7c
Thanh Dat Vu <vuthanhdat181@gmail.com>
2024-04-02 22:11:09 +0700

    Add ApplicationUser Repository

---
+++ Bulky.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    //Bcause I know exactly what is the class inside that category, that is Category
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+       
+    }
+}
