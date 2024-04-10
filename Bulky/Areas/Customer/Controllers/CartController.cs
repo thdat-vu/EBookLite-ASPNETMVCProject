99601070d21bd7fe7b03f341bbb9862d5053125f
Thanh Dat Vu <vuthanhdat181@gmail.com>
2024-04-10 12:20:49 +0700

    Update modify quantity in shopping cart

--- Bulky/Areas/Customer/Controllers/CartController.cs
+++ Bulky/Areas/Customer/Controllers/CartController.cs
@@ -12,7 +12,7 @@ namespace Bulky.Areas.Customer.Controllers
     [Authorize]
     public class CartController : Controller
     {
-        
+
         private readonly IUnitOfWork _unitOfWork;
         public ShoppingCartVM shoppingCartVM { get; set; }
         public CartController(IUnitOfWork unitOfWork)
@@ -39,18 +39,72 @@ namespace Bulky.Areas.Customer.Controllers
             return View(shoppingCartVM);
         }
         /// <summary>
-        /// This function return price of each cart based on quantity
+        /// This method plus 1 quantity of selected item and reload Index page
+        /// </summary>
+        /// <param name="cartId"></param>
+        /// <returns></returns>
+        public IActionResult Plus(int cartId)
+        {
+            //retrive the cart
+            var cartfromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+            //add 1 quantity
+            cartfromDb.Count += 1;
+            //update cart
+            _unitOfWork.ShoppingCart.Update(cartfromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        /// <summary>
+        /// This method minus 1 quantity of selected item and reload Index page
+        /// </summary>
+        /// <param name="cartId"></param>
+        /// <returns></returns>
+        public IActionResult Minus(int cartId)
+        {   //retrive the cart
+            var cartfromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+
+            //be careful of Count of the product is 1 -> Minus 1 means delete the cart
+            if (cartfromDb.Count <= 1)
+            {
+                //remove the product from cart
+                _unitOfWork.ShoppingCart.Remove(cartfromDb);
+
+            }
+            else
+            {
+                //minus the cart.Count
+                cartfromDb.Count -= 1;
+                //update the cart
+                _unitOfWork.ShoppingCart.Update(cartfromDb);
+            }
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Remove(int cartId)
+        {
+            //retrieve the cart
+            var cartfromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+            //remove the cart
+            _unitOfWork.ShoppingCart.Remove(cartfromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        /// <summary>
+        /// This function return price of each item based on quantity
         /// ex: FORTUNE OF TIME cart: quantity from 1-50: 90$, 51-100: 85$, 100+: 80$
         /// </summary>
         /// <param name="shoppingCart"></param>
         /// <returns></returns>
         private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
-            if(shoppingCart.Count <= 50)
+            if (shoppingCart.Count <= 50)
             {
                 return shoppingCart.Product.Price;
             }
-            else if(shoppingCart.Count <= 100)
+            else if (shoppingCart.Count <= 100)
             {
                 return shoppingCart.Product.Price50;
 
